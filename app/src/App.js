11a7fe7d61cbb23342807a948a89c33c79fec5fa
debauchery1st <trevor.j.martin@gmail.com>
2020-01-14 14:52:33 -0500

    GET on componentDidMount

--- app/src/App.js
+++ app/src/App.js
@@ -1,25 +1,24 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import axios from "axios";
+// import logo from "./logo.svg";
+// import "./App.css";
 
 class App extends React.Component {
+  state = {
+    baseURL: "https://api.github.com/users/debauchery1st",
+    data: []
+  };
+
+  componentDidMount() {
+    axios
+      .get(this.state.baseURL)
+      .then(result => this.setState({ data: result.data }));
+  }
   render() {
+    console.log(this.state.data);
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>gh</h1>
       </div>
     );
   }
