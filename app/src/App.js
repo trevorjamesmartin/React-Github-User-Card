9d68870e9d611e05165460baa34e5dbf32c539ed
Trevor Martin <trevor.j.martin@gmail.com>
2021-02-24 13:13:42 -0500

    Update App.js

--- app/src/App.js
+++ app/src/App.js
@@ -37,7 +37,7 @@ class App extends React.Component {
           <Route
             path="/"
             exact
-            component={() => <Redirect to={`/${process.env.USERNAME}/keepers`} />}
+            component={() => <Redirect to={`/${process.env.USERNAME || "trevorjmartin" }/keepers`} />}
           />
         </Switch>
       </div>
